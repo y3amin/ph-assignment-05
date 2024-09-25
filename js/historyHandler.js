3af90b3b580ad2f5a7af4c8eab24a62f03476ae2
Yeamin <cse2110328@adust.edu.bd>
2024-09-25 10:58:53 +0600

    historyHandler UI updated

--- js/historyHandler.js
+++ js/historyHandler.js
@@ -11,14 +11,15 @@ function recordDonationHistory(amount, cardTitle) {
     "p-4",
     "border",
     "border-gray-300",
-    "rounded",
+    "rounded-lg",
     "bg-white"
   );
 
   // Setting the inner HTML
   historyEntry.innerHTML = `
-        <p>${amount} Taka has been donated for ${cardTitle}</p>
-        <p>Date: ${formattedDate}</p>
+        <p class="font-bold p-2">${amount} Taka has been donated for ${cardTitle}</p>
+        <div class="my-2"></div>
+        <p class="bg-gray-100 p-2 rounded-lg">Date: ${formattedDate}</p>
     `;
 
   // Append
